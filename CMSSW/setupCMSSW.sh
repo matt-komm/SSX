109f2728dd9957f86b324150818587b4bd6ce0d8
Matthias <mkomm@cern.ch>
2016-11-27 17:33:39 +0100

    update version

--- CMSSW/setupCMSSW.sh
+++ CMSSW/setupCMSSW.sh
@@ -30,7 +30,7 @@ cd $BASEDIR/CMSSW
 export SCRAM_ARCH=slc6_amd64_gcc530
 addVar SCRAM_ARCH $SCRAM_ARCH
 
-CMSSWVERSION="CMSSW_8_0_12"
+CMSSWVERSION="CMSSW_8_0_24"
 
 execute "scramv1 project CMSSW $CMSSWVERSION"
 cd $CMSSWVERSION/src
@@ -38,9 +38,15 @@ cd $CMSSWVERSION/src
 eval `scramv1 runtime -sh`
 execute "git cms-init"
 
-#execute "git cms-merge-topic ikrav:egm_id_7.4.12_v1"
+execute "git cms-merge-topic ikrav:egm_id_80X_v1"
 
-execute "git clone -b CMSSW_8012 https://github.com/matt-komm/EDM2PXLIO.git"
+#execute "scram b -j10"
+#cd $CMSSW_BASE/external/$SCRAM_ARCH
+#execute "git clone https://github.com/ikrav/RecoEgamma-ElectronIdentification.git data/RecoEgamma/ElectronIdentification/data"
+#cd data/RecoEgamma/ElectronIdentification/data
+#execute "checkout egm_id_80X_v1"
+
+execute "git clone -b CMSSW_8024 https://github.com/matt-komm/EDM2PXLIO.git"
 execute "git reset EDM2PXLIO"
 execute "git clone https://github.com/matt-komm/Pxl.git"
 execute "git reset Pxl"
